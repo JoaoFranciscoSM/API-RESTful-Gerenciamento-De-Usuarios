b36d123a056340c5ff0da833e589677057c8b828
João Francisco Santos de Matos <124510955+JoaoFranciscoSM@users.noreply.github.com>
2024-01-08 12:14:55 -0300

    Delete gerenciamento-de-clientes directory

--- gerenciamento-de-clientes/src/main/java/francisco/joao/gerenciamentodeclientes/dominio/usuarios/UsuarioRecord.java
+++
@@ -1,9 +0,0 @@
-package francisco.joao.gerenciamentodeclientes.dominio.usuarios;
-
-public record UsuarioDTO(
-        Long id,
-        String email,
-        String senha,
-        TipoUsuario tipoUsuario
-) {
-}
